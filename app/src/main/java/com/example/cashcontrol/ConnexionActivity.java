0a36d3a3d263c825766925f3d4777707b7bfcf8a
Yassine <yhamidi@iut.univ-paris8.fr>
2023-04-08 17:15:13 +0200

    patch bug connexion

--- app/src/main/java/com/example/cashcontrol/ConnexionActivity.java
+++ app/src/main/java/com/example/cashcontrol/ConnexionActivity.java
@@ -39,7 +39,11 @@ public class ConnexionActivity extends AppCompatActivity {
     /*User*/
     private DatabaseUser dbUser;
 
+    private Boolean verifConnexion;
 
+    private Boolean mdp;
+
+    private String mdpHashDansLaBdd;
 
 
     @Override
@@ -61,7 +65,7 @@ public class ConnexionActivity extends AppCompatActivity {
 
 
         //On creer le handler avec le execute
-        if(handler == null)
+        if (handler == null)
             handler = FourniseurHandler.creerHandler();
 
         mConnexion_text_view_s_inscrire.setOnClickListener(view -> {
@@ -70,12 +74,13 @@ public class ConnexionActivity extends AppCompatActivity {
         });
 
         //On verifie si tous les champs sont remplit pour qu'on puisse appuer sur le bouton save
-        EditText[] editTexts = {mConnexion_champ_identifiant,mConnexion_mot_de_passe}; // Ajoutez tous vos EditText ici
+        EditText[] editTexts = {mConnexion_champ_identifiant, mConnexion_mot_de_passe}; // Ajoutez tous vos EditText ici
         for (EditText editText : editTexts) {
             editText.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 }
+
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
                     tousremplis = true;
@@ -85,17 +90,16 @@ public class ConnexionActivity extends AppCompatActivity {
                             tousremplis = false;
                         }
                     }
-                    if(tousremplis){
+                    if (tousremplis) {
                         mButtonConnexion.setEnabled(true);
-                    }
-                    else {
+                    } else {
                         mButtonConnexion.setEnabled(false);
                     }
                 }
+
                 @Override
                 public void afterTextChanged(Editable s) {
                 }
-
 
 
             });
@@ -105,39 +109,57 @@ public class ConnexionActivity extends AppCompatActivity {
                 String identifiant = mConnexion_champ_identifiant.getText().toString();
                 String motdepasse = mConnexion_mot_de_passe.getText().toString();
 
+                //Threads pour ne pas bloquer le thread principale, toute les grosses opérations de la BDD
+                FournisseurExecutor.creerExecutor().execute(() -> {
+                    verifConnexion = dbUser.verificationConnexionDansLaBDD(identifiant);
 
+                    mdpHashDansLaBdd = dbUser.verifMdpIdentifiant(identifiant);
+                    if (!mdpHashDansLaBdd.equals("")) {
+                        mdp = checkPassword(motdepasse, dbUser.verifMdpIdentifiant(identifiant));
+                        if (verifConnexion && mdp) {
+                            // post -> ajoute les instructions à la suite de celles du main thread
+                            handler.post(() -> {
+                                // Notification du main thread pour qu'il mette à jour la UI
+                                // tout ce qui touche à la UI doit être exécuté dans le main thread
+                                Toast.makeText(getApplicationContext(), "Bienvenue " + identifiant, Toast.LENGTH_SHORT).show();
+                            });
 
-                Boolean mdp = checkPassword(motdepasse, encrypt(motdepasse));
+                            int id = dbUser.retourneIdUser(identifiant);
 
+                            if (id != -1) {
+                                //on change la valeur dans les shared preferences
+                                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                                        .edit()
+                                        .putInt(SHARED_PREF_USER_INFO_ID, id)
+                                        .apply();
 
+                                Intent intent = new Intent(ConnexionActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            } else {
+                                handler.post(() -> {
 
+                                    Toast.makeText(getApplicationContext(), "Impossible de trouvé l'utilisateur" + identifiant, Toast.LENGTH_SHORT).show();
+                                });
+                            }
 
-                if (dbUser.verificationConnexionDansLaBDD(identifiant) && mdp) {
-                    Toast.makeText(getApplicationContext(), "Bienvenue " + identifiant, Toast.LENGTH_SHORT).show();
-
-                    int id = dbUser.retourneIdUser(identifiant);
-
-                    if(id != -1){
-                        //on change la valeur dans les shared preferences
-                        getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
-                                .edit()
-                                .putInt(SHARED_PREF_USER_INFO_ID, id)
-                                .apply();
-
-                        Intent intent = new Intent(ConnexionActivity.this, HomeActivity.class);
-                        startActivity(intent);
+                        } else {
+                            handler.post(() -> {
+                                Toast.makeText(getApplicationContext(), "Connexion impossible" + identifiant, Toast.LENGTH_SHORT).show();
+                            });
+                        }
                     }
-                    else {
-                        Toast.makeText(getApplicationContext(), "Impossible de trouvé l'utilisateur " + identifiant, Toast.LENGTH_SHORT).show();
+
+                    else{
+                        handler.post(() -> {
+                            Toast.makeText(getApplicationContext(), "Compte inexistant" + identifiant, Toast.LENGTH_SHORT).show();
+                        });
                     }
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Connexion impossible " + identifiant, Toast.LENGTH_SHORT).show();
-                }
+                });
             });
+
+
         }
     }
-
 
     public static boolean checkPassword(String password, String hashedPassword) {
         if (hashedPassword == null || !hashedPassword.startsWith("$2a$")) {
@@ -145,10 +167,4 @@ public class ConnexionActivity extends AppCompatActivity {
         }
         return BCrypt.checkpw(password, hashedPassword);
     }
-
-
-
-
-
-
 }
